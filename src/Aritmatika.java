d3ac2fbda4f47fa1ee09fc619a21e0bad4bb1fde
krossmanzs <radi.adi78@gmail.com>
2022-04-12 23:48:13 +0700

    Aritmatika dan Comparison

---
+++ src/Aritmatika.java
@@ -0,0 +1,73 @@
+public class Aritmatika {
+    // operator pada java adalah
+    // + = tambah
+    // - = kurang
+    // * = kali
+    // / = bagi
+    // % = modulus
+    // ++ = increment
+    // -- = decrement
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = 8;
+        int result;
+
+        // tambah
+        result = a + b;
+        System.out.println("Nilai a + b: " + result); // print line
+
+        result = b + c; // 10
+        System.out.printf("Nilai a + b: %d %s\n", result, "<- adalah hasilnya"); // print format
+
+        // kurang
+        a = 2;
+        b = 2;
+        result = a - b;
+//        System.out.println("Nilai " + a + " - " + b + " = " + result);
+        System.out.printf("Nilai %d - %d = %d\n", a, b, result);
+
+        // perkalian
+        a = 144;
+        b = 12;
+        result = a * b;
+        System.out.printf("Nilai %d * %d = %d\n", a, b, result);
+
+        // pembagian
+        float e = 8f;
+        float d = 8000000f;
+        float result2 = e / d;
+        System.out.printf("Nilai %f / %f = %f\n", e, d, result2);
+
+        System.out.printf("Nilai %f / %f = %f\n", 1f, 0f, (1f / 0f));
+
+        // modulus
+        int z = 5;
+        int x = 2;
+        int result3 = 5 % 2;
+        System.out.printf("Nilai %d %% %d = %d\n", z, x, result3);
+
+        // increment
+        int angka = 1;
+        System.out.printf("angka = %d\n", angka);
+        angka++; // angka = angka + 1
+        System.out.printf("angka = %d\n", angka);
+        angka += z; // angka = angka + 5
+        System.out.printf("angka = %d\n", angka);
+
+        // decrement
+        angka = 10;
+        System.out.printf("angka = %d\n", angka);
+        angka--; // angka = angka - 1
+        System.out.printf("angka = %d\n", angka);
+        angka -= 11; // angka = angka - 11
+        System.out.printf("angka = %d\n", angka);
+
+        // kalkultor sederhana
+        System.out.println("----KALKULATOR SEDERHANA----");
+        float angka1 = 23.3f;
+        float angka2 = 5.53f;
+        System.out.printf("%f + %f = %f\n", angka1, angka2, (angka1 + angka2));
+    }
+}
---
+++ src/Comparison.java
@@ -0,0 +1,46 @@
+public class Comparison {
+    // untuk membandingkan dua buah nilai/value
+    // dan mengembalikannya dalam bentuk boolean (true/false)
+
+    // 5 > 3 = true
+
+    // +----------+-----------------------------+------------+
+    // | operator |         penjelasan          | penggunaan |
+    // +----------+-----------------------------+------------+
+    // | ==       | sama dengan                 | x == y     |
+    // | !=       | tidak sama dengan           | x != y     |
+    // | >        | lebih besar dari            | x > y      |
+    // | <        | lebih kecil dari            | x < y      |
+    // | >=       | lebih besar atau sama dari  | x >= y     |
+    // | <=       | lebih kecil atau sama dari  | x <= y     |
+    // +----------+-----------------------------+------------+
+
+    public static void main(String[] args) {
+        System.out.println("====== Sama Dengan");
+        // sama dengan
+        System.out.printf("Nilai %d == %d : %b\n", 5, 5, (5 == 5));
+        System.out.printf("Nilai 2 == 3 : %b\n", (2 == 3));
+
+        System.out.println("====== Tidak Sama Dengan");
+
+        // tidak sama dengan
+        System.out.printf("Nilai 5 != 5 : %b\n", (5 != 5));
+        System.out.printf("Nilai 123 != 321 : %b\n", (123 != 321));
+
+        // lebih besar dari
+        System.out.println("====== Lebih Besar Dari");
+        System.out.printf("Nilai 2 > 12 : %b\n", (2 > 12));
+
+        // lebih besar sama dengan
+        System.out.println("====== Lebih Besar Sama Dengan");
+        System.out.printf("Nilai 2 >= 12 : %b\n", (2 >= 12));
+
+        // lebih kecil dari
+        System.out.println("====== Lebih Kecil Dari");
+        System.out.printf("Nilai 2 < 12 : %b\n", (2 < 12));
+
+        // lebih kecil sama dengan
+        System.out.println("====== Lebih Kecil Sama Dengan");
+        System.out.printf("Nilai 2 <= 12 : %b\n", (2 <= 12));
+    }
+}
