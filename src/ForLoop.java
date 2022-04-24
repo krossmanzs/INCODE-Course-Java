81cbb5febde6821d63691f21ddacb912053ff3e2
krossmanzs <radi.adi78@gmail.com>
2022-04-25 00:13:08 +0700

    Challange: Membuat belah ketupan dengan for loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -2,7 +2,7 @@ public class ForLoop {
     // looping adalah perulangan yang mengulangi
     // suatu kode sebanyak kali yang ditentukan
     // contoh:
-    // for(statement_pertama, statement_kedua, statement_ketiga){
+    // for(statement_pertama; statement_kedua; statement_ketiga){
     //     kode_yang_mau_diulangi;
     // }
 
@@ -15,10 +15,6 @@ public class ForLoop {
 
         // programmer biasanya pakai variabel
         // i,j,k
-
-        for (int i = 0; i < 10; i++) { // i = 10
-            System.out.println("Angka: " + i);
-        }
 
         for (int i = 0; i < 10; i++) { // i = 10
             System.out.println("Angka: " + i);
@@ -90,18 +86,80 @@ public class ForLoop {
         *****
          */
 
+        // i = 3 ||
         for(int i = 1; i <= 5; i++) {
             // spasi
-            for (int j = 5; j >= i + 1; j--) {
-                System.out.printf(" ");
+            for (int j = 5; j >= i + 1; j--) { // 54 - 2 perulangan - print 2 buah _
+                System.out.printf("_");
             }
 
             // bintang
-            for(int k = 1; k <= i; k++) {
+            for(int k = 1; k <= i; k++) { // 123 - 3 perulangan - print 3 buah *
                 System.out.printf("*");
             }
 
             System.out.println();
         }
+
+        System.out.println();
+
+        for(int i = 5; i >= 1; i--) {
+            System.out.println("ini loop i ke-" + i);
+            for(int j = 1; j <= i; j++) {
+                System.out.printf("\tini loop j ke-%d pada loop i ke-%d\n",j,i);
+            }
+
+            for(int anjay = 1; anjay <= 3; anjay++) {
+                System.out.printf("\tini loop anjay ke-%d pada loop i ke-%d\n",anjay,i);
+            }
+
+            System.out.println();
+        }
+
+        /*
+        1 2 3 4 5 6 7
+        1 2 3 _ 5 6 7
+        1 2 _ _ _ 6 7
+        1 _ _ _ _ _ 7
+        1 2 _ _ _ 6 7
+        1 2 3 - 5 6 7
+        1 2 3 4 5 6 7
+         */
+
+        System.out.println();
+        size = 101;
+        int a = 1, b = size / 2; // 3
+        boolean balik = false;
+        for (int i = 1; i <= size; i++) {
+            // untuk print 1-7 pada baris pertama dan terakhir
+            if(i == 1 || i == size) {
+                for(int j = 1; j <= size; j++) {
+                    System.out.printf("* ", j);
+                }
+            } else {
+                for(int j = 1; j <= b; j++) {
+                    System.out.printf("* ", j);
+                }
+                for(int j = 1; j <= a; j++) {
+                    System.out.printf("  ");
+                }
+                for(int j = a + b + 1; j <= size; j++) {
+                    System.out.printf("* ");
+                }
+            }
+
+            System.out.println();
+            if(i != 1 && i != size) {
+                if((a + 2 >= size) || balik) {
+                    a-=2;
+                    b++;
+                    balik = true;
+                } else {
+                    b--;
+                    a+=2;
+                }
+            }
+        }
+
     }
 }
