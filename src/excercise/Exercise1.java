bd8b913122a24d144a80686462419438d483e761
krossmanzs <radi.adi78@gmail.com>
2022-06-09 22:53:29 +0700

    Tik Tak Toe Selesaiiii

--- src/excercise/Exercise1.java
+++ src/excercise/Exercise1.java
@@ -1,37 +1,110 @@
 package excercise;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Exercise1 {
     public static void main(String[] args) {
         char[][] simbol = createNewSimbol();
 
-        // TODO: 6/4/2022 exception handling
-        // TODO: 6/4/2022 input selang seling antara o dan x
-        // TODO: 6/4/2022 cek kemenangan minimal saat loop ke 3 dan seterusnya
+        System.out.println("TIK TAK TO GEM");
+        printTicTacToe(simbol);
 
         // untuk dapetin data sebanyak 9 kali
         for (int i = 0; i < 9; i++) {
-            simbol = inputUser(simbol);
+            char huruf;
+            // genap o
+            if(i % 2 == 0) {
+                huruf = 'o';
+            } else { // ganjil x
+                huruf = 'x';
+            }
+
+            simbol = inputUser(simbol, huruf);
+
             printTicTacToe(simbol);
+
+            boolean menang;
+            if (i >= 2) {
+                menang = cekKemenangan(simbol, huruf);
+                if (menang) {
+                    System.out.printf("Huruf %c memenangkan permainan!\n", huruf);
+                    break;
+                }
+            }
+
+            if (i == 8) {
+                System.out.println("Tidak ada yang menang!");
+            }
         }
 
     }
-    static char[][] inputUser(char[][] simbol) {
-        // masukkan koordinat x: 5
 
+    private static boolean cekKemenangan(char[][] simbol, char huruf) {
+        // diagonal \
+        if ((simbol[0][0] == huruf) && (simbol[1][1] == huruf) && (simbol[2][2] == huruf)) {
+            return true;
+        }
+
+        // diagonal /
+        if ((simbol[2][0] == huruf) && (simbol[1][1] == huruf) && (simbol[0][2] == huruf)) {
+            return true;
+        }
+
+        // sejajar
+
+        int panjang = simbol.length;
+
+        // traverse ke kanan
+
+        int sama = 0;
+        for(int y = 0; y < panjang; y++) {
+            for(int x = 0; x < panjang; x++) {
+                if (simbol[x][y] == huruf) {
+                    sama++;
+                }
+            }
+
+            if (sama < panjang) {
+                sama = 0;
+            } else { // jika terdapat 3 huruf sejajar yang sama maka kita return true
+                return true;
+            }
+        }
+
+        // traverse ke bawah
+        for(int x = 0; x < panjang; x++) {
+            for(int y = 0; y < panjang; y++) {
+                if (simbol[x][y] == huruf) {
+                    sama++;
+                }
+            }
+
+            if (sama < panjang) {
+                sama = 0;
+            } else { // jika terdapat 3 huruf sejajar yang sama maka kita return true
+                return true;
+            }
+        }
+
+
+        // jika semua persyaratan kemenangan tidak memenuhi
+        // maka kita return false
+        return false;
+    }
+
+    static char[][] inputUser(char[][] simbol, char huruf) {
+        // index 1 = x
+        // index 0 = y
+        // masukkan koordinat
+        System.out.printf("===== (%c)\n", huruf);
         int koordinat[] = getKoordinat(simbol);
-        // index 0 = x
-        // index 1 = y
-        int x = koordinat[0];
-        int y = koordinat[1];
+        int x = koordinat[1];
+        int y = koordinat[0];
 
-        // simpen char temp
-        char temp = getSimbol();
+        simbol[x][y] = huruf;
 
-        simbol[x][y] = temp;
-
-        System.out.printf("Koordinat (%d,%d) diisi dengan %c\n", x, y, temp);
+        System.out.printf("Koordinat (%d,%d) diisi dengan %c\n", x, y, huruf);
 
         return simbol;
     }
@@ -59,18 +132,16 @@ public class Exercise1 {
     }
 
     static int[] getKoordinat(char[][] simbol) {
-        // index 0 = x
-        // index 1 = y
+        // index 1 = x
+        // index 0 = y
         int[] koordinat = new int[2];
-        Scanner userInput = new Scanner(System.in);
 
         do {
-            System.out.printf("masukkan koordinat x :");
-            koordinat[0] = userInput.nextInt();
-            System.out.printf("masukkan koordinat y :");
-            koordinat[1] = userInput.nextInt();
+            koordinat[0] = askKoordinat("masukkan koordinat x(0-2) :");
+            koordinat[1] = askKoordinat("masukkan koordinat y(0-2) :");
 
             boolean adaNilai = cekKoordinat(koordinat, simbol);
+
             if (adaNilai) {
                 System.err.println("\nKoordinat ini sudah diisi, silahkan masukkan ulang!");
             }
@@ -79,27 +150,30 @@ public class Exercise1 {
         return koordinat;
     }
 
-    static char getSimbol() {
-        // masukkan simbolnya(o/x)
-        boolean ulang = true;
-        char simbol;
+    static int askKoordinat(String message) {
+        boolean lanjut = true;
+        int angka = -99;
+        
         do {
             Scanner userInput = new Scanner(System.in);
-            System.out.printf("Masukkan simbolnya(o/x):");
-
-            simbol = userInput.next().charAt(0); // o
-
-            // kalo simbol == o/x maka ulang selesai
-            if(simbol == 'o' || simbol == 'x') {
-                ulang = false;
+            System.out.printf(message);
+            
+            try {
+                angka = userInput.nextInt();
+            } catch (InputMismatchException e) {
+                System.err.println("Silahkan masukkan angka yaaa!");
             }
 
-            if(ulang) {
-                System.err.println("\nMasukin yang bener blog >:(");
+            // angka 0-2
+            if (angka >= 0 && angka < 3) {
+                lanjut = false;
+            } else {
+                System.err.println("Koordinat melebihi jangkauan!");
             }
-        } while (ulang);
+            
+        } while (lanjut);
 
-        return simbol;
+        return angka;
     }
 
     static char[][] createNewSimbol() {
@@ -122,10 +196,14 @@ public class Exercise1 {
                 System.out.printf("%c", simbol[x][y]);
                 if(y != simbol[x].length - 1) {
                     System.out.printf(" | ");
+                } if (y == simbol[x].length - 1) {
+                    System.out.printf("\t %d", x);
                 }
             }
             if(x != 2) {
                 System.out.printf("\n============\n");
+            } if (x == 2) {
+                System.out.printf("\n %d   %d   %d \n", 0,1,2);
             }
         }
         System.out.println("\n");
