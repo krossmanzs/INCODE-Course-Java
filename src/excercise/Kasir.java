625bdb1508937ab0dc482307531fb4d1912dd7ef
krossmanzs <radi.adi78@gmail.com>
2022-06-12 21:48:38 +0700

    Add new exercise kasir

---
+++ src/excercise/Kasir.java
@@ -0,0 +1,33 @@
+package excercise;
+
+import java.util.ArrayList;
+
+public class Kasir {
+    // list produk
+    // nambah/update produk
+    // kasi harga setiap produk
+    // jumlah pembelian
+    // stok
+    // keuntungan
+
+    // Pisang x 1 : 5000
+    // Apel x 5 : 4000
+
+    // === pilih user
+    // 1. sebagai pembeli
+    // 2. sebagai penjual
+
+    // === Pembeli
+    // 1. beli barang
+    //      -> barangnya apa
+    //         kalau dia ga kosong kita beli
+    // 2. balik ke menu utama
+
+    // === Penjual
+    // 1. Nambahin barang
+    // 2. ngapus barang
+    // 3. cek keuntungan
+    // 4. balik ke menu utama
+
+
+}
